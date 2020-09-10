7e0ee1599da3c90bc9c43cf4abbe69bcdbbec720
vmadmin <vmadmin@bmLP1.smartlearn.wan>
2020-09-10 10:09:06 +0200

    2222

---
+++ Block3a/script.js
@@ -0,0 +1,29 @@
+document.write("Hello World with External file");
+
+document.write("<h1>Datatypes</h1>");
+
+//datatype Beispiele
+document.write("var x = 16 + 'Volvo'");
+document.write("<br>");
+var x = 16 + "Volvo";
+document.write(x);
+
+document.write("var x = '16'+ 'VOlvo'");
+document.write("<br>");
+x= "16"+ "Volvo";
+document.write(x);
+
+//Aufgabe 1
+document.write("<br>");
+document.write("<h1>Aufgabe 1</h1>")
+
+for (var i =1; i<20;i++){
+    
+    var x ="hallo";
+    document.write("<br>");
+    var result = x.fontsize(i);
+    document.write(result);
+    
+
+}
+
